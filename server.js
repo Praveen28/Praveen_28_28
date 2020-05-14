bef508ed4cbf7bf7e91a583526e1d9a48fbdf9ac
Praveen S <acgpraveen@gmail.com>
2020-05-14 22:29:23 +0530

    first commit

--- server.js
+++ server.js
@@ -20,12 +20,11 @@ mongo.connect(function (err, client) {
 
 app.use("/", routes);
 
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 

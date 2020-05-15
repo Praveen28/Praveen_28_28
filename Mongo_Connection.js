26363917de65cfe304c98f830912c2aa10a524e6
Praveen S <acgpraveen@gmail.com>
2020-05-15 18:28:03 +0530

    hello

--- Mongo_Connection.js
+++ Mongo_Connection.js
@@ -1,7 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 
 const url =
-  process.env.MONGODB_URI ||
   "mongodb://praveen:praveen@cluster0-shard-00-00-qpqiv.mongodb.net:27017,cluster0-shard-00-01-qpqiv.mongodb.net:27017,cluster0-shard-00-02-qpqiv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 async function connect(callback) {
